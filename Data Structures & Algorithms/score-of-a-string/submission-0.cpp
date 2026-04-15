6eef5f547f0efb9e03567fe593884bf72e28196c
Sonny <thaison08052007z@gmail.com>
2026-04-15 21:15:59 +0700

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0; 
+        for(int i = 0; i < s.size() - 1; i += 1){
+            sum += abs(s[i+1] - s[i]);
+        }
+        return sum;
+    }
+};
